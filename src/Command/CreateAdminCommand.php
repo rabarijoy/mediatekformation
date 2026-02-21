4a0383f36b5ac53c2b2c862b73a1ab706045ca47
Rabarijaona AJ <aj@MacBook-Air-de-Rabarijaona.local>
2026-02-21 19:40:11 +0300

    feat: entité user
    Fixes #5

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Crée un utilisateur administrateur en base de données'
+)]
+class CreateAdminCommand extends Command
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly UserPasswordHasherInterface $passwordHasher
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $user = new User();
+        $user->setEmail('admin@mediatekformation.fr');
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $hashedPassword = $this->passwordHasher->hashPassword($user, 'admin');
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $output->writeln('Utilisateur admin créé : admin@mediatekformation.fr / admin');
+
+        return Command::SUCCESS;
+    }
+}
